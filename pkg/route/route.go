cf8c56cb85ad82115e6bd1ccdaf9daf6d969d871
Tirapol Jansiricharoen <tjansirichar@Tirapols-MacBook-Air.local>
2023-10-10 23:29:59 +0700

    Build basic getting review by ID feature

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -3,6 +3,9 @@ package route
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 
 	"food-review/pkg/db"
 	"food-review/pkg/model"
@@ -30,6 +33,33 @@ func (h *Handler) GetAllReviews(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = h.Template.ExecuteTemplate(w, "reviews.html", allReviews)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+}
+
+func (h *Handler) GetReview(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	reviewIDstr := mux.Vars(r)["reviewID"]
+	reviewIDu64, err := strconv.ParseUint(reviewIDstr, 10, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	db := h.ReviewDB.GetDB()
+	targetReview, err := model.GetReview(db, uint(reviewIDu64))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	err = h.Template.ExecuteTemplate(w, "review.html", targetReview)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
